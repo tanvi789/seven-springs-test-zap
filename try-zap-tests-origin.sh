8000096bba4e10bd68e2dd93de5f504d27545e3f
tanvi <tanvi.nanda@valtech.co.uk>
2019-04-12 01:47:20 +0100

    update

--- try-zap-tests-origin.sh
+++ try-zap-tests-origin.sh
@@ -6,6 +6,8 @@ BUILD_NUMBER="1"
 
 VERSION="1.0"
 
-zap-api-scan.py -t \
+docker pull owasp/zap2docker-weekly
+
+docker run --rm -v $(pwd):/zap/wrk/:rw owasp/zap2docker-weekly zap-api-scan.py -t \
 $baseurl -f openapi \
 -g gen.conf -r zap-scan-${BUILD_NUMBER}-release-version-${VERSION}.html
