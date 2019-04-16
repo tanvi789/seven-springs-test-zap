8291e715e67e824201ca2393e227c54adfd506ec
Tanvi Nanda <tanvinanda@tanvis-macbook.home>
2019-04-16 23:44:13 +0100

    updated

--- try-zap-tests.sh
+++ try-zap-tests.sh
@@ -2,10 +2,14 @@
 
 baseurl="https://www.example.com/openapi.json"
 
+echo $baseurl
+
+echo ${baseurl}
+
 BUILD_NUMBER="1"
 
 VERSION="1.0"
 
 zap-api-scan.py -t \
-$baseurl -f openapi \
+${baseurl} -f openapi \
 -g gen.conf -r zap-scan-${BUILD_NUMBER}-release-version-${VERSION}.html
